9d66c01a643619ee183405247620da480ffc4cd7
Ethan Mcintyre <mcintyea@mail.uc.edu>
2024-10-07 11:42:17 -0400

    Complete

--- src/Main.java
+++ src/Main.java
@@ -5,74 +5,101 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String Player1 = "";
         String Player2 = "";
-        boolean donePlay1 = false;
-        boolean donePlay2 = false;
+        boolean playAgain = false;
 
         do {
-            System.out.print("Player 1 enter your move [RPS]: ");
-            Player1 = in.nextLine();
-            if (Player1.equals("R") || Player1.equals("P") || Player1.equals("S"))
+            boolean donePlay1 = false;
+            boolean donePlay2 = false;
+
+            do {
+                System.out.print("Player 1 enter your move [RPS]: ");
+                Player1 = in.nextLine();
+                if (Player1.equals("R") || Player1.equals("P") || Player1.equals("S"))
+                {
+                    donePlay1 = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input! You must enter RPS not " + Player1);
+                }
+            } while (!donePlay1);
+
+            do {
+                System.out.print("Player 2 enter your move [RPS]: ");
+                Player2 = in.nextLine();
+                if (Player2.equals("R") || Player2.equals("P") || Player2.equals("S"))
+                {
+                    donePlay2 = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input! You must enter RPS not " + Player2);
+                }
+            } while (!donePlay2);
+
+            if (Player1.equals("R") && Player2.equals("R"))
             {
-                donePlay1 = true;
+                System.out.println("Rock vs Rock it's a tie!");
+            }
+            else if (Player1.equals("R") && Player2.equals("S"))
+            {
+                System.out.println("Rock breaks Scissors! Player 1 wins!");
+            }
+            else if (Player1.equals("S") && Player2.equals("P"))
+            {
+                System.out.println("Scissors cuts Paper! Player 1 wins!");
+            }
+            else if (Player1.equals("S") && Player2.equals("S"))
+            {
+                System.out.println("Scissors vs Scissors it's a tie!");
+            }
+            else if (Player1.equals("P") && Player2.equals("R"))
+            {
+                System.out.println("Paper covers Rock! Player 1 wins!");
+            }
+            else if (Player1.equals("P") && Player2.equals("P"))
+            {
+                System.out.println("Paper vs Paper it's a tie!");
             }
             else
             {
-                System.out.println("Invalid input! You must enter RPS not " + Player1);
+                if (Player2.equals("R") && Player1.equals("S")) {
+                    System.out.println("Rock breaks Scissors! Player 2 wins!");
+                }
+                else if (Player2.equals("S") && Player1.equals("P"))
+                {
+                    System.out.println("Scissors cuts Paper! Player 2 wins!");
+                }
+                else if (Player2.equals("P") && Player1.equals("R"))
+                {
+                    System.out.println("Paper covers Rock! Player 2 wins!");
+                }
             }
-        } while (!donePlay1);
 
-        do {
-            System.out.print("Player 2 enter your move [RPS]: ");
-            Player2 = in.nextLine();
-            if (Player2.equals("R") || Player2.equals("P") || Player2.equals("S"))
-            {
-                donePlay2 = true;
-            }
-            else
-            {
-                System.out.println("Invalid input! You must enter RPS not " + Player2);
-            }
-        } while (!donePlay2);
+            playAgain = false;
+            while (true) {
 
-        if (Player1.equals("R") && Player2.equals("R"))
-        {
-            System.out.println("Rock vs Rock it's a tie!");
-        }
-        else if (Player1.equals("R") && Player2.equals("S"))
-        {
-            System.out.println("Rock breaks Scissors! Player 1 wins!");
-        }
-        else if (Player1.equals("S") && Player2.equals("P"))
-        {
-            System.out.println("Scissors cuts Paper! Player 1 wins!");
-        }
-        else if (Player1.equals("S") && Player2.equals("S"))
-        {
-            System.out.println("Scissors vs Scissors it's a tie!");
-        }
-        else if (Player1.equals("P") && Player2.equals("R"))
-        {
-            System.out.println("Paper covers Rock! Player 1 wins!");
-        }
-        else if (Player1.equals("P") && Player2.equals("P"))
-        {
-            System.out.println("Paper vs Paper it's a tie!");
-        }
-        else
-        {
-            if (Player2.equals("R") && Player1.equals("S"))
-            {
-                System.out.println("Rock breaks Scissors! Player 2 wins!");
+                System.out.print("Do you want to play again? [Y/N]: ");
+                String response = in.nextLine().trim().toLowerCase();
+
+                if (response.equals("y"))
+                {
+                    playAgain = true;
+                    break;
+                }
+                else if (response.equals("n"))
+                {
+                    playAgain = false;
+                    break;
+                }
+                else
+                {
+                    System.out.println("Invalid input! Please enter Y or N.");
+                }
             }
-            else if (Player2.equals("S") && Player1.equals("P"))
-            {
-                System.out.println("Scissors cuts Paper! Player 2 wins!");
-            }
-            else if (Player2.equals("P") && Player1.equals("R"))
-            {
-                System.out.println("Paper covers Rock! Player 2 wins!");
-            }
-        }
+
+        } while (playAgain);
 
     }
 }
+
